5e57f200fcda08f1b86d1a916ea112bdf22f5115
orkhan123 <orkhan@gmail.com>
2025-03-04 19:27:46 +0100

    create account implemented

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("*** Bankomata xosh gelmisiz ***");
+        NewAccount newAccount = new NewAccount();
+        while(true){
+           int secim =0;
+           System.out.println("Zehmet olmasa secim edin (1,2,3,4,5,6):"
+                   + "\n1. Qeydiyyat"
+                   + "\n2. Deposit money"
+                   + "\n3. Withdraw money"
+                   + "\n4. Balansi goster"
+                   + "\n5. Pin deyis"
+                   + "\n6. Hesabi sil");
+           if(scanner.hasNextInt()){
+               secim = scanner.nextInt();
+               switch(secim){
+                   case 1:{
+                       newAccount.register();
+                       break;
+                   }
+                   default:
+                       System.out.println("Girdiyiniz secim duzgun deyil");
+                       break;
+               }
+           }
+           else{
+               System.out.println("Yanlis sorgu");
+               scanner.next();
+           }
+       }
     }
 }
